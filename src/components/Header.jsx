c07c0125cf3794df73fcd3a44dc5a8d8d211796c
maiBass <maibassbluegrass@gmail.com>
2024-05-10 12:16:35 +0900

    import react-icons into header jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,37 @@
-export const Header = () => {
-  const componentName = () => 'Header';
+// import images
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
 
+//import libraries
+import { FaTwitter,FaGithub } from 'react-icons/fa';
+
+export const Header = () => {
   return (
-    <div className="test-wrapper">
-      This component name is {componentName()} .
-    </div>
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+       {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+        <div className="container">
+          <div className="display-table">
+            <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            <h1 className="title-text">maiBass</h1>
+            <h3 className="title-text">ツイ廃</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                <a href="https://twitter.com/subhuman_2">
+                  <FaTwitter color="white" size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                <a href="https://github.com/maibasshub">
+                  <FaGithub color="white" size="2rem" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+export const Skills = () => {
+  return (
+    <div id="skills">
+      <div className="conrtainer">
+        <div className="heading">
+          <h2>Skills</h2>
+        </div>
+        <div className="skills-container">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
